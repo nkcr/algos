6122f537e6b591cab4e67b55f8d8c2f5dc9ddff0
Noémien Kocher <nkcr.je@gmail.com>
2015-04-24 13:34:55 +0200

    design

--- touchscreen.rb
+++ touchscreen.rb
@@ -19,11 +19,11 @@ def three_calibration(
   fraction = 1/(a0*b1-a0*b2-a1*b0+a1*b2+a2*b0-a2*b1).to_f
 
   a = fraction * (-x1*b0+x2*b0+x0*b1-x2*b1-x0*b2+x1*b2)
-  b = fraction * (x1*a0-x2*a0-x0*a1+x2*a1+x0*a2-x1*a2)
+  b = fraction * ( x1*a0-x2*a0-x0*a1+x2*a1+x0*a2-x1*a2)
   c = fraction * (-x2*a1*b0+x1*a2*b0+x2*a0*b1-x0*a2*b1-x1*a0*b2+x0*a1*b2)
 
   d = fraction * (-y1*b0+y2*b0+y0*b1-y2*b1-y0*b2+y1*b2)
-  e = fraction * (y1*a0-y2*a0-y0*a1+y2*a1+y0*a2-y1*a2)
+  e = fraction * ( y1*a0-y2*a0-y0*a1+y2*a1+y0*a2-y1*a2)
   f = fraction * (-y2*a1*b0+y1*a2*b0+y2*a0*b1-y0*a2*b1-y1*a0*b2+y0*a1*b2)
   return [a,b,c,d,e,f]
 end
